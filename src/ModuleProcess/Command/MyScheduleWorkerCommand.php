6de19e5be31363636295172a98855025ab3cf94a
Alexander Ionov <magneticsun@gmail.com>
2026-01-29 14:13:14 +0300

    v.0.06 myscheduleworker

---
+++ src/ModuleProcess/Command/MyScheduleWorkerCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\ModuleProcess\Command;
+
+use Cron\CronExpression;
+use App\Message\SimpleMessage;
+//use Doctrine\DBAL\Connection;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+#[AsCommand(name: 'app:schedule:worker', description: 'Add a short description for your command')]
+class MyScheduleWorkerCommand extends Command
+{
+	public function __construct(private MessageBusInterface $bus)
+	{
+		parent::__construct();
+	}
+	protected function execute(InputInterface $input, OutputInterface $output): int
+	{
+		$now = new \DateTimeImmutable();
+
+		/*
+		 $jobs = $this->db->fetchAllAssociative("
+		 SELECT id, cron_expr, payload
+		 FROM scheduled_jobs
+		 WHERE active = 1
+		 ");
+		 */
+
+		$jobs = [
+				[
+						'id' => 1,
+						'cron_expr' => '* * * * *'
+				],
+				[
+						'id' => 2,
+						'cron_expr' => '* * * * *'
+				]
+		];
+
+		foreach ( $jobs as $job )
+		{
+			$cron = CronExpression::factory($job['cron_expr']);
+			//$cron = CronExpression::factory('* * * * *');
+
+			if ($cron->isDue($now))
+			{
+				//$this->bus->dispatch(new SimpleMessage($job['payload']));
+
+				$output->writeln("Queued job {$job['id']}");
+			}
+		}
+
+		return Command::SUCCESS;
+	}
+}
